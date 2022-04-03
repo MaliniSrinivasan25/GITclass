0c5bce1e145645c35a0e2ee4f04e233c3e2bdcd7
MaliniSrinivasan25 <malini.srinivasan25@outlook.com>
2022-04-03 15:10:43 +0530

    Modified Class1

--- src/com/reg/Class1.java
+++ src/com/reg/Class1.java
@@ -1,5 +1,14 @@
 package com.reg;
 
 public class Class1 {
-
+	
+	public void method1() {
+		System.out.println("Name");
+	}
+	public void method2() {
+		System.out.println("Name2");
+	}
+	public void method3() {
+		System.out.println("Name3");
+	}
 }
