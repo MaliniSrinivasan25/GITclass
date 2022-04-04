b17e663ba3671f0e0c5ed52b10c46b820b15b50d
MaliniSrinivasan25 <malini.srinivasan25@outlook.com>
2022-04-04 09:25:43 +0530

    Added Global code

--- src/com/reg/Class1.java
+++ src/com/reg/Class1.java
@@ -11,6 +11,9 @@ public class Class1 {
 	public void method3() {
 		System.out.println("Name3");
 	}
+	public void method4() {
+		System.out.println("Name4");
+	}
 	public static void main(String[] args) {
 		Class1 c = new Class1();
 		c.method1();
